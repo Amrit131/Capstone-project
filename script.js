45d480084cce3a26eb1b52cf31beeeafcc8b074e
Amrit131 <amrit.raex@gmail.com>
2022-10-25 00:27:13 +0530

    creating Navbar & hamburger button

---
+++ script.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+const close = document.getElementsByClassName('hamburger-container')[0];
+
+function toggle() {
+  close.classList.toggle('close');
+}
